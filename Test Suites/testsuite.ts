1be77445f1acdcd29e8ea8571a3492fee8b9db5c
partheep <partheepbommina@gmail.com>
2021-03-19 14:23:43 +0530

    my first commit test suite with result

---
+++ Test Suites/testsuite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testsuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8d5e1e5b-3268-409f-a53f-3e2102eed144</testSuiteGuid>
+   <testCaseLink>
+      <guid>a148f82d-45f0-4665-b8ea-cca972991ef6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testgoogle</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
